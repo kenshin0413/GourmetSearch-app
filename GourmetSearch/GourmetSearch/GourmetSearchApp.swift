9faa38556784b77f15f6dddffd2d1764a97d03e2
kenshin0413 <kenshin1234569@gmail.com>
2026-01-18 13:02:39 +0900

    初期

---
+++ GourmetSearch/GourmetSearch/GourmetSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GourmetSearchApp.swift
+//  GourmetSearch
+//
+//  Created by miyamotokenshin on R 8/01/18.
+//
+
+import SwiftUI
+
+@main
+struct GourmetSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
